408d81596c08e1ae425580b797aaec395b6d3a1a
Mike Pfeiffer <mike.pfeiffer@gmail.com>
2020-01-15 08:08:25 -0700

    Update UnitTest.ps1

--- tests/UnitTest.ps1
+++ tests/UnitTest.ps1
@@ -3,17 +3,17 @@ param (
 )
 
 $template = Get-Content $TemplatePath | convertfrom-json
+$nsg = $template.resources | Where-Object type -eq 'Microsoft.Network/networkSecurityGroups'
 
 Describe 'NSG Validation' {
 	Context 'Check NSG Rules' {
         It "Verify SSH is not open to the internet" {
-            $nsg = $template | Where-Object type -eq 'Microsoft.Network/networkSecurityGroups'
-            $nsg.properties.securityRules.properties.destinationPortRange | Should Not Be 22            
+            $nsg.properties.securityRules.properties.destinationPortRange | Should -Not -Be 22            
         }
         It "Verify RDP is not open to the internet" {
-          $nsg = $template | Where-Object type -eq 'Microsoft.Network/networkSecurityGroups'
-          $nsg.properties.securityRules.properties.destinationPortRange | Should Not Be 3389            
+          $nsg.properties.securityRules.properties.destinationPortRange | Should -Not -Be 3389          
       }        
     }
 }
+
 
